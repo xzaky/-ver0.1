84167a560d8adedada1def742041a9ec01e99975
xzaky <47222671+xzaky@users.noreply.github.com>
2025-02-06 14:06:39 +0300

    Add files via upload

--- Новый текстовый документ (3).js
+++ Новый текстовый документ (3).js
@@ -1,9 +1,13 @@
-const num1=+prompt("Введите первое число:"),
-      num2=+prompt("Введите второе число:"),
-      op=prompt("Введите операцию которую хотите выполнить:");
+const op=prompt("Введите операцию которую хотите выполнить:+,-,/,*."),
+      num1=+prompt("Введите первое число:"),
+      num2=+prompt("Введите второе число:");
 if(op=="+"){
-alert(num1+num2);
-}else{
-alert(num1-num2);
+alert(`${num1}+${num2}=${num1+num2}`);
+}else if(op=="-"){
+alert(`${num1}-${num2}=${num1-num2}`);
+}else if(op=="/"){
+alert(`${num1}/${num2}=${num1/num2}`);
+}else if(op=="*"){
+alert(`${num1}*${num2}=${num1*num2}`);
 }
 
